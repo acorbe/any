57f1f1cac4b997efe274566b3e2affb823f11114
Alessandro C <corbisoft@gmail.com>
2019-09-19 13:25:07 +0200

    commands in a list test the default before using any

--- any-bash.sh
+++ any-bash.sh
@@ -3,11 +3,42 @@ if [ "$ANY_ALIAS_CD" = true ]; then
    alias acd='any cd'
 fi
 
+# any will try the default behavior for these commands before trying its expansion.
+ANY_TEST_DEFAULT_BEHAVIOR_FIST="^(cd|ls|cat|less|more)$"
+
+
 function any (){
     for arg__ in $@; do :; done #only portable way
     target_pattern=$arg__
-    #echo "target pattern" $target_pattern, "pwd" $(pwd)    
-    #target_file=`(find . -maxdepth 1 -name *$target_pattern* | tail -n 1)`
+
+    command_=$1
+
+    if [[ "$ANY_DEBUG" == true ]]; then
+	echo "command:" $command_
+    fi
+    
+    if [[ "$command_" =~ $ANY_TEST_DEFAULT_BEHAVIOR_FIST ]]; then
+	if [[ "$ANY_DEBUG" == true ]]; then
+	    echo "matching! - trying:" ${@:1}
+	fi
+
+	#test_def_beh=$(eval ${@:1}) #2> /dev/null
+	#echo "def behavior" $test_def_beh
+
+	eval ${@:1} 2> /dev/null
+	
+	if [[ $? -eq 0 ]]; then
+	    if [[ "$ANY_DEBUG" == true ]]; then
+		echo "default behavior ok"
+	    fi
+	    return 0
+	else
+	    if [[ "$ANY_DEBUG" == true ]]; then
+		echo "rerouting behavior"
+	    fi
+	fi
+    fi
+    
 
     # https://stackoverflow.com/q/23356779/1714661
     array=()
@@ -36,9 +67,6 @@ function any (){
 		echo -e "expanded to: \e[34m${@:1:$#-1} $target_file\033[0m"
 		eval ${@:1:$#-1} $target_file
 		break;		
-	    done	    	
-	    
+	    done
     esac
-    
-    
- }
+}
