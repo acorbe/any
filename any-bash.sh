317de9a6c84dc474804222296ca08f047253ec3b
Alessandro C <corbisoft@gmail.com>
2019-11-05 08:55:48 +0100

    improved documentation, now any supports a syntax in which also intermediate path elements are expanded

--- any-bash.sh
+++ any-bash.sh
@@ -17,7 +17,11 @@ ANY_COMMANDS_FOR_DIR_ONLY_SEARCH="^(cd|cd)$"
 
 ANY_ARCHITECTURE_FOR_FIND_COMMAND=`uname`
 
-function any_find_command () {
+#from https://stackoverflow.com/a/17841619/1714661
+#join strings by separator
+function join_by { local IFS="$1"; shift; echo "$*"; }
+
+function any_find_command_basic () {
     ## results are presented in reverse cronological order
     ## see https://superuser.com/a/608889/164234
     target_pattern=$1
@@ -38,66 +42,94 @@ function any_find_command () {
     
 }
 
-function any_find_command_experimental () {
+function any_find_command () {
+    ## Any supports a syntax with trailing slashes that are themselves expanded.
+    ## Not used on Darwin
+    ## In case of expansion, -type d is suppressed
+    
     target_pattern=$1
     any_find_type_restrict=$2
 
-    ## correct maxdepth
-    
-    ## -path <low folder
-    ## \( -and \)
-
-    ## first we determine whether the string needs to be split
-    #if [[ ".*/.*" ~= $ ]]
-
-    #array=()
-    #while IFS=  read -r -d "/"; do
-   # 	echo "${REPLY}"
-   #	array+=("${REPLY}")
-   # 	# done < <(find . -maxdepth 1 -iname "*$target_pattern*" $any_find_type_restrict -print0 -exec echo '{}' +) #
-    #done < $1
-
-    ## quantifies the number of arguments
-    # target_max_d=`python -c "print(len(\"$target_pattern\".split(\"/\")))"`
-    # target_max_path=`python -c "print(   \"/\".join(  map(lambda x : \"*\" + x + \"*\",   \"$target_pattern\".split(\"/\")[:-1]  )  )       )"`
-
-    # echo $target_max_d
-    # echo $target_max_path
-
     if [[ "$ANY_DEBUG" == true ]]; then
-	echo "Notice - this is the experimental any find call."
+	{
+	    echo "[ANY] - Notice - this is the experimental any find call."
+	    echo "[ANY] - The received pattern is: ${target_pattern}"
+	} >&2
     fi
 
 
     #check if it's a path with / in between.
-    echo "Defining wheter the path contains at least one '/'"
+    if [[ "$ANY_DEBUG" == true ]]; then
+	>&2 echo "[ANY] - Defining wheter the path contains at least one '/'"
+    fi
+    
     if [[ "${target_pattern}" =~ "/" ]]; then
 	if [[ "$ANY_DEBUG" == true ]]; then
-	    echo "Detected / in path. Any searches in composed paths."
+	    >&2 echo "[ANY] - Detected / in path. Any searches in composed paths."
 	fi
-	IFS='/' array=($target_pattern)
-	echo ${array}
-
-    
-    else
+	IFS='/' path_split=($target_pattern)
+	path_len_subelements=${#path_split[@]}
 	
+	joined_patternized_path=""
+	for element in "${path_split[@]:0:$path_len_subelements-1}"
+	do
+	    if [[ "$ANY_DEBUG" == true ]]; then
+		>&2 echo "[ANY] - tokenized path part: $element adding ->" '*'"${element}"'*'
+	    fi
+	    #local_patternized_path_el="*${element}*"
+	    #patternized_path_el+=('*'"${element}"'*')
+	    joined_patternized_path="${joined_patternized_path}/"'*'"${element}"'*'
+	done
+	joined_patternized_path="${joined_patternized_path:1}"
+	#joined_patternized_path=$(join_by "/" ${patternized_path_el[@]} )
+
 	if [[ "$ANY_DEBUG" == true ]]; then
-	    echo "NOT Detected / in path. Any searches in composed paths."
+	    >&2 echo "[ANY] - updated -maxdepth" $path_len_subelements
+	    >&2 echo "[ANY] - updated -path" "${joined_patternized_path}"
+	    >&2 echo "[ANY] - updated -iname" "*${path_split[$path_len_subelements-1]}*"
 	fi	
 	
 	#find which comes with macos has different flags and less opts.
 	if [[ "$ANY_ARCHITECTURE_FOR_FIND_COMMAND" == "Darwin" ]]; then
 	    find . -maxdepth 1 $any_find_type_restrict\
 		 -iname "*$target_pattern*" \
-		  -print0 -exec echo '{}' +
+		 -print0 -exec echo '{}' +
+	else
+	    if [[ "$ANY_DEBUG" == true ]]; then
+		>&2 echo "[ANY] - FIND CALL: find "-maxdepth $path_len_subelements \
+		 \( -path "${joined_patternized_path}" \) \
+		 -iname "*${path_split[$path_len_subelements-1]}*" \
+		 -printf '%Ts\t%p\0'
+	    fi
+	    find . -maxdepth $path_len_subelements \
+		 \( -path "${joined_patternized_path}" \) \
+		 -iname "*${path_split[$path_len_subelements-1]}*" \
+		 -printf '%Ts\t%p\0' 2> /dev/null | sort -nrz | cut -f2 -z #| xargs -0 ls
+	         #"${any_find_type_restrict}"
+	         #2> /dev/null
+		 #$any_find_type_restrict \
+	fi
+
+    
+    else
+	
+	if [[ "$ANY_DEBUG" == true ]]; then
+	    >&2 echo "[ANY] - NO / detected in path. Simple any search."
+	fi
+
+	#find which comes with macos has different flags and less opts.
+	if [[ "$ANY_ARCHITECTURE_FOR_FIND_COMMAND" == "Darwin" ]]; then
+	    find . -maxdepth 1 $any_find_type_restrict\
+		 -iname "*$target_pattern*" \
+		 -print0 -exec echo '{}' +
 	else    
 	    find . -maxdepth 1 \
 		 $any_find_type_restrict \
 		 -iname "*$target_pattern*" \
 		 -printf '%Ts\t%p\0' | sort -nrz | cut -f2 -z #| xargs -0 ls            
-	fi
+	fi	
 
-    fi
+    fi    
 }
 
 
@@ -112,18 +144,18 @@ function any (){
     command_=$1
 
     if [[ "$ANY_DEBUG" == true ]]; then
-	echo "command:" $command_
+	echo "[ANY] - command:" $command_
     fi
 
     #in case no command is passed, it will display a usage guide.
     if [[ -z "$command_" ]]; then	
-	echo "Any: keyword-based navigation, in bash."
-	echo "   Alessandro Corbetta, 2019"
+	echo "Any: keyword-based navigation, in bash."	
 	echo "USAGE:"
 	echo "   any <command> [flags...] <keyword>"
 	echo "EXAMPLE:"
 	echo "   any cd work"
 	echo "   will expand to, e.g., cd workspace, if there is only one match, or it will prompt a selection panel."
+	echo "Alessandro Corbetta, 2019"
 	return 
     fi
 
@@ -132,7 +164,7 @@ function any (){
     #behavior without any. Only if it fails the any machinery starts.
     if [[ "$command_" =~ $ANY_TEST_DEFAULT_BEHAVIOR_FIST ]]; then
 	if [[ "$ANY_DEBUG" == true ]]; then
-	    echo "cd in ANY_TEST_DEFAULT_BEHAVIOR_FIST. testing:" ${@:1}
+	    echo "[ANY] - Testing default behavior: " ${@:1}
 	fi
 
 	#Default behavior, with no stderr on display.
@@ -140,12 +172,12 @@ function any (){
 	
 	if [[ $? -eq 0 ]]; then
 	    if [[ "$ANY_DEBUG" == true ]]; then
-		echo "default behavior ok"
+		echo "[ANY] - default behavior ok"
 	    fi
 	    return 0
 	else
 	    if [[ "$ANY_DEBUG" == true ]]; then
-		echo "default_behavior failed. Rerouting to any."
+		echo "[ANY] - default_behavior failed. Rerouting to any."
 	    fi
 	fi
     fi
@@ -158,23 +190,26 @@ function any (){
 	any_find_type_restrict="-type f"
     else
 	if [[ "$ANY_DEBUG" == true ]]; then
-	    echo "command: $command_ does not match dir-only or file-only searches."
+	    echo "[ANY] - command: $command_ does not match dir-only or file-only searches."
 	fi
     fi
     if [[ "$ANY_DEBUG" == true ]]; then
-	echo "any_find_type_restrict: " $any_find_type_restrict
-	echo "passing to find function:" "${target_pattern}"
+	echo "[ANY] - any_find_type_restrict for command ${command_}:" $any_find_type_restrict
+	echo "[ANY] - passing to find function:" "${target_pattern}"
     fi
 
     if [[ "$ANY_FIND_COMMAND_EXP" == true ]]; then
 	ANY_FIND_COMMAND_FOO=any_find_command_experimental
 	if [[ "$ANY_DEBUG" == true ]]; then
-	    echo "any in experimental foo mode"
+	    echo "[ANY] - any in experimental foo mode"
 	fi
-	
-	echo "DUPLICATED CALL"
-	$ANY_FIND_COMMAND_FOO "${target_pattern}" "$any_find_type_restrict"
-	echo "END DUPLICATED CALL"
+
+	if [[ "$ANY_DEBUG" == true ]]; then
+	    echo "[ANY] -    Duplicated find call for testing {{"
+	    duplicated_output=$($ANY_FIND_COMMAND_FOO "${target_pattern}" "$any_find_type_restrict")
+	    echo "[ANY] -    Output from duplicated call:" $duplicated_output
+	    echo "[ANY] -    }} End duplicated call"
+	fi
 	
     else
 	ANY_FIND_COMMAND_FOO=any_find_command
